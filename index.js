7e5b6bb510ce513e3fb9cb7fe3255d2ffbd818a5
Lauren Harradine <lhharradine@gmail.com>
2023-03-01 13:53:31 +1100

    Changed canvas width and height

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-// note that canvas is by default 300 x 150
 const canvas = document.querySelector('canvas')
 // c is for context!
 const c = canvas.getContext('2d')
+
+// Note that canvas is by default 300 x 150, so here I'm
+// setting 16:9 aspect ratio which should fit on most screens
+canvas.width = 1024
+canvas.height = 576
 
 c.fillStyle = 'white'
 c.fillRect(10, 10, 50, 50)
