95fd1024ef81abeda58ebf6cc00c6e1466566979
Lauren Harradine <lhharradine@gmail.com>
2023-03-01 13:50:44 +1100

    Added index.js and drew a rectangle

---
+++ index.js
@@ -0,0 +1,7 @@
+// note that canvas is by default 300 x 150
+const canvas = document.querySelector('canvas')
+// c is for context!
+const c = canvas.getContext('2d')
+
+c.fillStyle = 'white'
+c.fillRect(10, 10, 50, 50)
